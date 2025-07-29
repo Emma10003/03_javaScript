f62e62ff4ea48efe49635e15b456ae9b6f473571
유성 <5emma10003@gmail.com>
2025-07-29 12:18:43 +0900

    JSON데이터 실습 1차

---
+++ js/19_JSON데이터실습.js
@@ -0,0 +1,23 @@
+// 1. const elementById로 모두 HTML-JS 연결 설정하기
+const result = document.getElementById("result");
+const fetchPost = document.getElementById("fetchPost");
+const content = document.getElementById("content");
+
+// html button type은 submit 이 아니라 button 형태로 타입 맞춰주는 것이 바람직함.
+fetchPost.addEventListener("click", function () {
+    fetch("https://jsonplaceholder.typicode.com/posts/1")
+        .then((response) => {
+            response.json();
+            console.log(response);
+        }) // json 형태로 변환  parse() 메서드와 json() 메서드 차이 확인
+        .then((data) => {
+            console.log(data);
+            // innerHTML : <> 태그 적용      innerText : <> 태그도 텍스트로 출력
+            content.innerHTML = `
+            <strong>userID:</strong>${data.userId} <br>
+            <strong>userID:</strong>${data.id} <br>
+            <strong>userID:</strong>${data.title} <br>
+            <strong>userID:</strong>${data.body} <br>
+            `;
+        }); // data 자리는 그냥 변수 이름.
+});
